dc306b40919a34b83ad9827f6fa6409661075eb8
OlasubomiOmoloju <subby.omoloju@gmail.com>
2026-08-26 18:08:32 +0100

    backend server completed

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
-import express, { NextFunction, Request, response, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
 import accountRouter from "./routes/accountRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import activityRouter from "./routes/activityRoutes.js";
+import { initScheduler } from "./services/schedulerService.js";
 
 const app = express();
 
@@ -26,6 +28,10 @@ app.use("/api/auth", authRouter)
 app.use("/api/oauth", socialAuthRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/posts", postRouter)
+app.use("/api/activity", activityRouter)
+
+// Initialize Scheduler
+initScheduler()
 
 //Global Error Handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction)=>{
